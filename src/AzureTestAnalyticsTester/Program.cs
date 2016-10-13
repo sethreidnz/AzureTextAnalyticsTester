c96d78a28316d1bf7b8cd3ae36f8f4cd58117d69
Seth Reid <contact@sethreid.co.nz>
2016-10-13 13:19:26 +1300

    added basic console components

--- src/AzureTestAnalyticsTester/Program.cs
+++ src/AzureTestAnalyticsTester/Program.cs
@@ -1,14 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace AzureTestAnalyticsTester
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private static TextAnalyticsClient _textAnalyticsClient;
+
+        public static async void Main(string[] args)
         {
+           var config = BuildConfiguration();
+
+            var textAnalyticsKey = config["TextAnalytics:AzureSubscriptionKey"];
+            _textAnalyticsClient = new TextAnalyticsClient(textAnalyticsKey);
+
+            Console.WriteLine($"How are you feeling today?");
+            var response = Console.ReadLine();
+
+            Console.WriteLine($"You Responed with:");
+            Console.WriteLine(response);
+            var centimentScore = GetCentimentScore(response);
+
+            WaitKey();
+        }
+
+        private static async Task<decimal?> GetCentimentScore(string text)
+        {
+            decimal? centimentScore = null;
+            try
+            {
+                centimentScore = await _textAnalyticsClient.GetSentiment(text);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"There was an error calling the Azure Text Analytics api:");
+                Console.WriteLine(JsonConvert.SerializeObject(exception));
+            }
+            return centimentScore;
+        }
+
+        private static IConfigurationRoot BuildConfiguration()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddUserSecrets("text-analytics-tester");
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets("text-analytics-tester")
+                .Build();
+
+            return config;
+        }
+
+        private static void WaitKey()
+        {
+            Console.WriteLine("Press ESC to stop");
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    // Do something
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+
+        private static string GetConfigValue(string key)
+        {
+            return "";
         }
     }
 }
---
+++ src/AzureTestAnalyticsTester/TextAnalyticsClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AzureTestAnalyticsTester
+{
+    public class TextAnalyticsClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _autherisationCredentials;
+        public TextAnalyticsClient(string apiKey)
+        {
+            _autherisationCredentials = Base64Encode($"AccountKey:{apiKey}");
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<decimal> GetSentiment(string text)
+        {
+            return await Task.FromResult(0);
+        }
+
+        private static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+    }
+}
